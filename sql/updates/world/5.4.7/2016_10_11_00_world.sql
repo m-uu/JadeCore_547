84203cc45eac49075fd91bd2b646be062162c575
Ice.Net <mysterium@ice-wow.com>
2016-10-11 00:04:01 +0200

    Update 2016_10_11_00_world.sql

--- sql/updates/world/5.4.7/2016_10_11_00_world.sql
+++ sql/updates/world/5.4.7/2016_10_11_00_world.sql
@@ -17,7 +17,6 @@ INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `p
 DELETE FROM `gameobject_template` WHERE (entry = 205824);
 
 INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `faction`, `flags`, `size`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `data8`, `data9`, `data10`, `data11`, `data12`, `data13`, `data14`, `data15`, `data16`, `data17`, `data18`, `data19`, `data20`, `data21`, `data22`, `data23`, `data24`, `data25`, `data26`, `data27`, `data28`, `data29`, `data30`, `data31`, `unkInt32`, `AIName`, `ScriptName`, `WDBVerified`) VALUES
-
 (205824, 3, 6502, 'Sacred Receptacle', '', '', '', 0, 4, 1, 62267, 0, 0, 0, 0, 0, 1852, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', 15595);
 
 INSERT INTO `gameobject_loot_template` (`Entry`, `Item`, `ChanceOrQuestChance`, `LootMode`, `GroupId`, `MinCountOrRef`, `MaxCount`) VALUES (205824, 62267, -100, 1, 0, 1, 1);
