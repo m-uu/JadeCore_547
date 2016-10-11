d8d2a95625fe084543891ebba9ea832c52580dca
Lisandro Díaz Delgado <lddelgado@uebcolon.hidro.cu>
2016-10-11 14:37:22 -0500

    Create  	2016_10_11_01_world.sql
    Kirin Tor Beacon and Sunreaver Beacon Teleport in isle of Thunder

---
+++ sql/updates/world/5.4.7/ 	2016_10_11_01_world.sql
@@ -0,0 +1,4 @@
+INSERT INTO `spell_scripts`  (id, effIndex, delay, command, datalong, datalong2, dataint, x, y, z, o) VALUES
+(140295, 0, 1, 6, 1064, 0, 0, "6133,24", "5017,16", "36,89", "1,422");
+INSERT INTO spell_scripts (id, effIndex, delay, command, datalong, datalong2, dataint, x, y, z, o) VALUES
+(140300, 0, 1, 6, 1064, 0, 0, "7246,79", "6261,95", "19,3688", "2,5317");
