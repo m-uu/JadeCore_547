1adc5677db3cd3ed9ccf230cb63b8e836a1e70c1
Bodeguero <Bodeguero@users.noreply.github.com>
2016-10-11 19:27:51 -0400

    Travis Happy

--- sql/updates/world/5.4.7/2016_10_11_01_world.sql
+++ sql/updates/world/5.4.7/2016_10_11_01_world.sql
@@ -3,7 +3,7 @@
 
 DELETE FROM `spell_scripts`  WHERE (id = 140295);
 INSERT INTO `spell_scripts`  (id, effIndex, delay, command, datalong, datalong2, dataint, X, Y, z, o) VALUES
-(140295, 0, 1, 6, 1064, 0, 0, "6133,24", "5017,16", "36,89", "1,422");
+('140295', '0', '1', '6', '1064', '0', '0', '6133,24', '5017,16', '36,89', '1,422');
 DELETE FROM `spell_scripts`  WHERE (id = 140300);
 INSERT INTO `spell_scripts` (id, effIndex, delay, command, datalong, datalong2, dataint, X, Y, z, o) VALUES
-(140300, 0, 1, 6, 1064, 0, 0, "7246,79", "6261,95", "19,3688", "2,5317");
+('140300', '0', '1', '6', '1064', '0', '0', '7246,79', '6261,95', '19,3688', '2,5317');
